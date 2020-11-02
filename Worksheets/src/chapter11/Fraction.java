bfe368fa7f09a32c38aefca3f5f49641f954bf22
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2020-11-02 16:37:08 +0800

    Add files via upload

---
+++ Worksheets/src/chapter11/Fraction.java
@@ -0,0 +1,112 @@
+package chapter11;
+
+/*
+ * Chapter 11 of the Java Worksheets, with improvements
+ * 
+ */
+
+public class Fraction implements Comparable<Fraction>
+{
+	private int num;
+	private int den;
+
+	public Fraction()
+	{
+	}
+	
+	// Constructor method
+	public Fraction(int num, int den) throws Exception 
+	{
+		if( den == 0 )
+			throw new Exception("Division by zero");
+		
+		this.num = num;
+		this.den = den;
+		this.simplify();
+	}
+
+	public int getNum()
+	{
+		return num;
+	}
+
+	public void setNum(int num)
+	{
+		this.num = num;
+	}
+
+	public int getDen()
+	{
+		return den;
+	}
+
+	public void setDen(int den)
+	{
+		this.den = den;
+	}
+
+	// this is redundant, but it is an exaple of encapsulation
+	public void print()
+	{
+		System.out.println(this.num + "/" + this.den);
+	}
+
+	// 
+
+	private static int gcd(int num1, int num2)
+	{
+		int gcd = 1;
+		while (num1 != num2)
+		{
+			if (num1 > num2)
+			{	num1 = num1 - num2;
+				gcd = num1;
+			} else
+			{	num2 = num2 - num1;
+				gcd = num2;
+			}
+		}
+		return gcd;
+	}
+	
+	public void simplify()
+	{
+		int gcd = gcd(this.num, this.den);
+		this.num = this.num / gcd;
+		this.den = this.den / gcd;
+		
+	}
+	
+	public double toDouble()
+	{
+		return (double)this.num/this.den;
+	}
+	
+	//
+	// Homework: implement the add method!
+	
+	public Fraction add(Fraction b) throws Exception
+	{
+		int resultNum = this.num * b.den + this .den * b.num;
+		int resultDen = this.den * b.den;
+		return new Fraction(resultNum, resultDen);
+	}
+	
+	public int compareTo(Fraction b)
+	{
+		double comp = this.toDouble() - b.toDouble();
+		if( comp == 0)
+			return 0;
+		else if( comp < 0 )
+			return -1;
+		else
+			return 1;
+	}
+	
+	// polymorphism
+	public String toString()
+	{
+		return this.num + "/" + this.den;
+	}
+
+}
---
+++ Worksheets/src/chapter11/FractionTest.java
@@ -0,0 +1,73 @@
+package chapter11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class FractionTest
+{
+
+	// We moved the enter (Fraction) method to this class, to keep...
+	// the Fraction class I/O-free
+	// input a numerator and a denominator to make a fraction
+	public static Fraction enterFraction() throws Exception
+	{
+		// note: no format check, though!!!
+		String strFraction = IBIO.input("Enter fraction (a/b format) ");
+		strFraction = strFraction.replace("/", " "); // Changes slash to space
+		Scanner parse = new Scanner(strFraction); // to parse the fraction
+		int a = parse.nextInt(); // so we can extract the numerator
+		int b = parse.nextInt(); // denominator
+		parse.close();
+		Fraction f = new Fraction(a, b);
+		return f;
+	}
+	
+	public static void listAsDouble(ArrayList<Fraction> fal)
+	{
+		for(Fraction f : fal)
+		{
+			System.out.printf("%.2f  ", f.toDouble());
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Fraction f = new Fraction(2, 10);
+		f.print();
+		// Making the enter method static allows us to call it
+		// before/in order to create a Fraction object
+		boolean error = false;
+		Fraction g = new Fraction();
+		do
+		{
+			try
+			{
+				g = enterFraction();
+			} catch( Exception e )
+			{
+				error = true;
+				System.out.println( e.getMessage() );
+			}
+		} while (error);
+		
+		System.out.println(g);
+		System.out.println(g.toDouble());
+		Fraction h = f.add(g);
+		Fraction i = h.add(g);
+		ArrayList<Fraction> fractionList = new ArrayList<>();
+		fractionList.add(f);
+		fractionList.add(i);
+		fractionList.add(h);
+		fractionList.add(g);
+		System.out.println(fractionList);
+		listAsDouble(fractionList);
+		Collections.sort(fractionList);
+		System.out.println(fractionList);
+		listAsDouble(fractionList);
+
+	}
+
+}
